150a1a4971cc5136edf8a61dbfd7b179baa321f8
Seth Holloway <smholloway@gmail.com>
2010-12-21 15:30:58 -0600

    added name field for users

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,5 +1,7 @@
 class UserController < ApplicationController
   def show
+    @user = User.find(params[:id])
+    # If this show page is only for the currently logged in user change it to @user = current_user
   end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,23 @@
 class User < ActiveRecord::Base
+  has_many :goals
+  has_many :tasks
+  has_many :achievements
+  
   # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable, :lockable and :timeoutable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  # https://github.com/plataformatec/devise/wiki/How-To:-Allow-users-to-sign_in-using-their-username-or-email-address
+  attr_accessor :login
+
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me
+  attr_accessible :login, :name, :email, :password, :password_confirmation, :remember_me
   
-  has_many :goals
-  has_many :tasks
-  has_many :achievements
+  protected
+
+    def self.find_for_database_authentication(conditions)
+      value = conditions[authentication_keys.first]
+      where(["name = :value OR email = :value", { :value => value }]).first
+    end
 end
