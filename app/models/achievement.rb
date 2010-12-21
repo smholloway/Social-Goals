9ca8024668985540afe57514ae08abaa78cf717f
Shayne Czyzewski <thelastemail@gmail.com>
2010-12-21 10:27:45 -0600

    Added getting most recent of goals/achievements. SMC

--- app/models/achievement.rb
+++ app/models/achievement.rb
@@ -2,11 +2,17 @@ class Achievement < ActiveRecord::Base
   belongs_to :user
   belongs_to :level
   
-  validates_presence_of :user_id, :level_id
-  validates_associated  :user, :level
+  validates_presence_of   :user_id, :level_id
+  validates_uniqueness_of :user_id, :scope => :level_id # prevents duplicate achievements
+  validates_associated    :user, :level
   validate :must_be_active
-
+  
   def must_be_active
     errors.add_to_base("This achievement is not active") unless level.achievement_type.active
+  end
+  
+  def self.most_recent(limit_amount=5, public_only=true)
+    return self.all(:conditions => ["public = ?", public_only],
+                    :limit => limit_amount, :order => "created_at DESC")
   end
 end
--- app/models/goal.rb
+++ app/models/goal.rb
@@ -4,4 +4,9 @@ class Goal < ActiveRecord::Base
   
   validates_presence_of :name, :user_id
   validates_associated  :user
+  
+  def self.most_recent(limit_amount=5, public_only=true)
+    return self.all(:conditions => ["public = ?", public_only],
+                    :limit => limit_amount, :order => "created_at DESC")
+  end
 end
