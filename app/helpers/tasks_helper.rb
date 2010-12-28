cc94d363177e619759f39abd67a0085972680e1a
Shayne Czyzewski <thelastemail@gmail.com>
2010-12-28 10:37:28 -0600

    Made the display of goals/tasks a bit nicer than the default scaffolding. SMC

--- app/helpers/tasks_helper.rb
+++ app/helpers/tasks_helper.rb
@@ -1,2 +1,10 @@
 module TasksHelper
+  def display_time_left(task)
+    if task.deadline < Date.today
+      time_left = 'was due ' + distance_of_time_in_words(Time.now, task.deadline, false) + ' ago'
+    elsif
+      time_left = 'will be due in ' + distance_of_time_in_words(Time.now, task.deadline, false)
+    end
+    time_left
+  end
 end
