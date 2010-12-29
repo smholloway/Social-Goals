dc55421cfa5e398eec6b8c1cc4b2c5fc90ddb351
Seth Holloway <smholloway@gmail.com>
2010-12-29 10:25:00 -0600

    prettied up the goals index page a bit

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -5,3 +5,14 @@
 	$('.private_goal_on').live('click', function(){
 		$(this).siblings('form').submit();
 	});
+	
+	$('.plus_minus').live('click', function(){
+		$(this).siblings('.task_list').toggle();
+		if ($(this).text() == '+') {
+			$(this).replaceWith('<div class="plus_minus">-</div>');
+		} else {
+			$(this).replaceWith('<div class="plus_minus">+</div>');
+		}
+		
+	});
+	
