e23443224008673e4a8568aa9c1f96f2e25085ea
Shayne Czyzewski <thelastemail@gmail.com>
2011-01-05 11:33:19 -0600

    added a user profile page with facebook style routing

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -3,5 +3,16 @@ class UserController < ApplicationController
     @user = User.find(params[:id])
     # If this show page is only for the currently logged in user change it to @user = current_user
   end
-
+  
+  def profile
+    @user = User.find_by_name(params[:name])
+    redirect_to root_url and return if @user.nil?
+    @goals = @user.goals.where(:public => true) # get all public goals for this username
+    
+    respond_to do |format|
+      format.html
+      format.js
+    end
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -26,7 +26,9 @@ AchieveAnything::Application.routes.draw do
   # Sample of regular route:
   #   match 'products/:id' => 'catalog#view'
   # Keep in mind you can assign values other than :controller and :action
-
+  
+  match '/:name' => 'user#profile' # shows usernames
+  
   # Sample of named route:
   #   match 'products/:id/purchase' => 'catalog#purchase', :as => :purchase
   # This route can be invoked with purchase_url(:id => product.id)
